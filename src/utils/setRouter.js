dd3b9d01a4f1c144236d465b1d3b7f7fbfe390eb
Davchick <davchick2000ort@gmail.com>
2024-01-23 15:14:07 +0300

    attempt #3

---
+++ src/utils/setRouter.js
@@ -0,0 +1,11 @@
+import path from "path";
+import express from "express";
+
+const app = express();
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "../../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../", "dist", "index.html"));
+});
